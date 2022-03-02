698fdf5e86907ff73e00e6bf3d27ca295fcff3d7
Shrey Makhesana <53891868+shreymac2411@users.noreply.github.com>
2022-03-02 10:31:15 -0500

    main page Commit

--- BTP400_LMS/src/application/Main.java
+++ BTP400_LMS/src/application/Main.java
@@ -1,10 +1,13 @@
 package application;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
 
 //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
@@ -17,13 +20,29 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
 		try {
-			Group root = new Group();
+			GridPane root = new GridPane();
+			root.setHgap(10);
+			root.setVgap(10);
+			root.setAlignment(Pos.CENTER);
 			Scene scene = new Scene(root, 480, 360);
 			stage.setScene(scene);
 			Image icon = new Image("mainIcon.png");
 			stage.setTitle("Library Management System");
 			stage.getIcons().add(icon);
 			stage.setResizable(false);
+
+			Label FL = new Label("Are you a :");
+			
+			Button adminButton = new Button("Admin");
+			
+			Button librarianButton = new Button("Librarian");
+			
+			Button studentButton = new Button("Student");
+			
+			root.addRow(0, FL);
+			root.addRow(1, adminButton);
+			root.addRow(2, librarianButton);
+			root.addRow(3, studentButton);
 			stage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
