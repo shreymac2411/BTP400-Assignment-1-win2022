aa67a11c7568a66171a95a705a7c45845495451a
Shrey Makhesana <53891868+shreymac2411@users.noreply.github.com>
2022-03-02 21:00:41 -0500

    update

--- BTP400_LMS/src/application/Main.java
+++ BTP400_LMS/src/application/Main.java
@@ -7,8 +7,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 
-//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-
 public class Main extends Application {
 
 	public static void main(String[] args) {
@@ -18,7 +16,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("splashScreen.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			Image icon = new Image("mainIcon.png");
