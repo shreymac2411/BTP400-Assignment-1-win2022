38608c10701305a8d27a6c8d1c9a9614a8340b36
mmaghakian <99614548+mmaghakian@users.noreply.github.com>
2022-03-08 17:09:05 -0500

    Add TestDB

--- BTP400_LMS/src/extra/TestDB.java
+++ BTP400_LMS/src/extra/TestDB.java
@@ -1,15 +1,12 @@
-package extra;
+package ca.seneca.btp400.library;
 
-import extra.table.Book;
-import extra.table.Librarian;
-import extra.table.Student;
+import ca.seneca.btp400.library.table.Book;
+import ca.seneca.btp400.library.table.Librarian;
+import ca.seneca.btp400.library.table.Student;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 /**
  * Test class for Database
@@ -18,31 +15,39 @@ public class TestDB {
 
 
     private static final Path studentPath = Paths.get("db", "students.txt");
-    private static Student studentTestData [] = {
-            new Student(1, "mark eco", new Date(), 0, "1234"),
-            new Student(2, "tom ley", new Date(), 9, "1234"),
-            new Student(3, "jim scott", new Date(), 15, "1234"),
-            new Student(5, "raven solomon", new Date(), 0, "1234"),
-            new Student(4, "tim patrick terry", new Date(), 3, "1234")
+    private static Student studentTestData[] = {
+            new Student(1, "mark eco", new Date().toString(), "1234"),
+            new Student(2, "tom ley", new Date().toString(), "1234"),
+            new Student(3, "jim scott", new Date().toString(), "1234"),
+            new Student(5, "raven solomon", new Date().toString(), "1234"),
+            new Student(4, "tim patrick terry", new Date().toString(), "1234")
     };
-    private static Book bookTestData [] = {};//TODO
-    private static Librarian librarianTestData [] = {};//TODO
 
-    public TestDB(){
+    private static Librarian libTestData[] = {
+        new Librarian(1, "1234", "Lib Rarian")
+    };
+
+    private static Book bookTestData[] = {
+            new Book(1, "Test1", "Test Publisher", "Test Author", new Date().toString(), 356, "test book", true),
+            new Book(2, "Test2", "Test Publisher", "Test Author", new Date().toString(), 184, "test book", false),
+            new Book(3, "Test3", "Test Publisher", "Test Author", new Date().toString(), 732, "test book", true),
+            new Book(4, "Test4", "Test Publisher", "Test Author", new Date().toString(), 221, "test book", false),
+            new Book(5, "Test5", "Test Publisher", "Test Author", new Date().toString(), 631, "test book", true),
+            new Book(6532, "bookbook", "Test Publisher", "Test Author", new Date().toString(), 523, "test book!!!", false),
+
+    };//TODO
+
+    public TestDB() {
 
         System.out.println("TestDB()!");
         Database d = new Database(true);
 
-        Set set = new HashSet<>();
-        set.addAll(List.of(studentTestData));
-        d.arrayToFile(set.toArray(new Object[0]), studentPath);
-        d.tableToString(studentPath);
+        //Set set = new HashSet<>();
+        //set.addAll(List.of(studentTestData));
+        //d.arrayToFile(set.toArray(new Object[0]), studentPath);
+        //d.arrayToFile(bookTestData, Database.getBookPath());
+        d.arrayToFile(libTestData, Database.getLibrarianPath());
 
     }
 
-    public TestDB(Database db) {
-        super();
-
-        db.viewItemTable();
-    }
 }
